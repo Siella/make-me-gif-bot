0abf663d0bb7c6b4e0bbe90c03b14a0cf6fcf431
Любовь Елховская <lyubov.elkhovskaya@gmail.com>
2022-03-26 01:52:45 +0300

    Update test_bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,9 +1,11 @@
+import re
 import time
 from unittest.mock import patch
 
 from telebot import types
 
 from source.bot import bot
+from source.config import MSG
 
 
 def capture_event(*args, **kwargs):
@@ -22,7 +24,7 @@ def check_reaction(text: str, capsys, msg=None):
     bot.process_new_messages([msg])
     time.sleep(1)
     captured = capsys.readouterr()
-    return captured.out
+    return re.sub('\\\\n', "\\n", captured.out)
 
 
 class FakeObj:
@@ -39,20 +41,19 @@ class FakeClient:
 @patch('telebot.TeleBot.send_message', side_effect=capture_event)
 class TestBot:
     def test_command_start(self, mock1, mock2, capsys):
-        assert "Send me one or more pictures" \
-               in check_reaction('/start', capsys)
+        assert MSG.start.format('test') in check_reaction('/start', capsys)
 
     def test_command_help(self, mock1, mock2, capsys):
-        assert "I can make a GIF" in check_reaction('/help', capsys)
+        assert MSG.help in check_reaction('/help', capsys)
 
     def test_command_download(self, mock1, mock2, capsys):
-        assert "Here you go!" in check_reaction('/download', capsys)
+        assert MSG.download in check_reaction('/download', capsys)
 
     def test_command_download_all(self, mock1, mock2, capsys):
-        assert "Type IDs of users" in check_reaction('/download_all', capsys)
+        assert MSG.download_all in check_reaction('/download_all', capsys)
 
     def test_text_handler(self, mock1, mock2, capsys):
-        assert "Try to type /start" in check_reaction('test', capsys)
+        assert MSG.text in check_reaction('test', capsys)
 
     def test_step_break_handler(self, mock1, mock2, capsys):
         from source.bot import process_photo_step, step_break_handler
@@ -60,27 +61,27 @@ class TestBot:
         msg = create_text_message('/restart')
         step_break_handler(process_photo_step(msg))
         captured = capsys.readouterr()
-        assert "Okay, I stop." in captured.out
+        assert MSG.restart in captured.out
 
     @patch('source.bot.process_photo', return_value=None)
     @patch('telebot.types.Message.parse_photo', return_value=[1])
     def test_process_photo_step(self, mock1, mock2, mock3, mock4, capsys):
-        from source.bot import USERS, process_photo_step
+        from source.bot import IMAGES, process_photo_step
 
         msg = create_text_message('main')
 
         process_photo_step(msg)
-        assert "Please, send me pictures" in check_reaction('test', capsys)
+        assert MSG.process_photo in check_reaction('test', capsys)
 
         msg.photo = [1]
         process_photo_step(msg)
-        assert "Upload another pictures" in check_reaction('', capsys, msg)
+        assert MSG.process_photo_done in check_reaction('', capsys, msg)
 
-        USERS[msg.from_user.id] = [1]
+        IMAGES[msg.from_user.id] = [1]
         msg.text = '/done'
         process_photo_step(msg)
-        del USERS[msg.from_user.id]
-        assert "Next, give me a some text" in check_reaction('', capsys, msg)
+        del IMAGES[msg.from_user.id]
+        assert MSG.process_photo_next in check_reaction('', capsys, msg)
 
     @patch('source.bot.process_font_type_step', return_value=None)
     def test_process_text_step(self, mock1, mock2, mock3, capsys):
@@ -88,11 +89,11 @@ class TestBot:
 
         msg = create_text_message('')
         process_text_step(msg)
-        assert "Please, send me some symbols" in check_reaction('', capsys)
+        assert MSG.process_text_repeat in check_reaction('', capsys)
 
         msg.text = 'test'
         process_text_step(msg)
-        assert "Choose a font" in check_reaction('', capsys)
+        assert MSG.font in check_reaction('', capsys)
 
     @patch('source.bot.process_font_size_step', return_value=None)
     def test_process_font_type_step(self, mock1, mock2, mock3, capsys):
@@ -100,11 +101,11 @@ class TestBot:
 
         msg = create_text_message('')
         process_font_type_step(msg)
-        assert "Choose a font" in check_reaction('', capsys)
+        assert MSG.font in check_reaction('', capsys)
 
         msg.text = '/arial'
         process_font_type_step(msg)
-        assert "Choose font size" in check_reaction('', capsys)
+        assert MSG.size in check_reaction('', capsys)
 
     @patch('source.bot.send_result_step', return_value=None)
     def test_process_font_size_step(self, mock1, mock2, mock3, capsys):
@@ -112,11 +113,11 @@ class TestBot:
 
         msg = create_text_message('')
         process_font_size_step(msg)
-        assert "Choose font size" in check_reaction('', capsys)
+        assert MSG.size in check_reaction('', capsys)
 
         msg.text = '/medium'
         process_font_size_step(msg)
-        assert "Okay, wait" in check_reaction('', capsys)
+        assert MSG.wait in check_reaction('', capsys)
 
     @patch('source.bot.client', return_value=FakeClient())
     @patch('source.bot.send_content', return_value=None)
@@ -128,13 +129,13 @@ class TestBot:
 
         msg = create_text_message('')
         send_result_step(msg)
-        assert "Type /publish (only for GIFs)" in check_reaction('', capsys)
+        assert MSG.finish in check_reaction('', capsys)
 
         obj = FakeObj()
         msg.text = '/publish'
         upload_result_step(msg, obj)
-        assert "I kept it!" in check_reaction('', capsys)
+        assert MSG.saved in check_reaction('', capsys)
 
         msg.text = '/save'
         upload_result_step(msg, obj)
-        assert "I kept it!" in check_reaction('', capsys)
+        assert MSG.saved in check_reaction('', capsys)
